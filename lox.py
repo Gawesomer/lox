a45bbbaee37bf1a5c88810189d08119527b16c4b
Jean-Baptiste Jouband <jean-baptiste@jouband.com>
2021-07-07 17:41:41 -0600

    Basic scanner implementation

---
+++ lox.py
@@ -0,0 +1,139 @@
+import sys
+
+from token import Token
+from token_type import TokenType
+
+
+class Lox:
+
+    had_error = False
+
+    @classmethod
+    def main(cls):
+        if len(sys.argv) > 2:
+          print("Usage: jlox [script]")
+          exit(1)
+        elif len(sys.argv) == 2:
+          cls.run_file(sys.argv[1])
+        else:
+          cls.run_prompt()
+
+
+    @classmethod
+    def run_file(cls, filename: str):
+        with open(filename, 'r') as f:
+            code = f.read()
+            cls.run(code)
+        if cls.had_error:
+            exit(1)
+
+
+    @classmethod
+    def run_prompt(cls):
+        while True:
+            try:
+                line = input("> ")
+                cls.run(line)
+                cls.had_error = False
+            except EOFError:
+                break
+
+
+    @classmethod
+    def run(cls, source: str):
+        scanner = Scanner(source)
+        tokens = scanner.scan_tokens()
+        for token in tokens:
+            print(token)
+
+
+    @classmethod
+    def error(cls, line: int, message: str):
+        cls.report(line, "", message)
+
+
+    @classmethod
+    def report(cls, line: int, where: str, message: str):
+        print("[line {}] Error{}: {}".format(line, where, message), file=sys.stderr)
+        cls.had_error = True
+
+
+class Scanner:
+
+    def __init__(self, source: str):
+        self.source = source
+        self.tokens = []
+
+        self.start = 0
+        self.current = 0
+        self.line = 1
+
+
+    def scan_tokens(self):
+        while not self.is_at_end():
+            # We are at the beginning of the next lexeme.
+            self.start = self.current
+            self.scan_token()
+
+        self.tokens.append(Token(TokenType.EOF, "", None, self.line))
+        return self.tokens
+
+
+    def scan_token(self):
+        c = self.advance()
+        if c == '(':
+            self.add_token(TokenType.LEFT_PAREN)
+        elif c == ')':
+            self.add_token(TokenType.RIGHT_PAREN)
+        elif c == '{':
+            self.add_token(TokenType.LEFT_BRACE)
+        elif c == '}':
+            self.add_token(TokenType.RIGHT_BRACE)
+        elif c == ',':
+            self.add_token(TokenType.COMMA)
+        elif c == '.':
+            self.add_token(TokenType.DOT)
+        elif c == '-':
+            self.add_token(TokenType.MINUS)
+        elif c == '+':
+            self.add_token(TokenType.PLUS)
+        elif c == ';':
+            self.add_token(TokenType.SEMICOLON)
+        elif c == '*':
+            self.add_token(TokenType.STAR)
+        elif c == '!':
+            self.add_token(TokenType.BANG_EQUAL if self.match('=') else TokenType.BANG)
+        elif c == '=':
+            self.add_token(TokenType.EQUAL_EQUAL if self.match('=') else TokenType.EQUAL)
+        elif c == '<':
+            self.add_token(TokenType.LESS_EQUAL if self.match('=') else TokenType.LESS)
+        elif c == '>':
+            self.add_token(TokenType.GREATER_EQUAL if self.match('=') else TokenType.GREATER)
+        else:
+            Lox.error(self.line, "Unexpected character.")
+
+
+    def is_at_end(self):
+        return self.current >= len(self.source)
+
+
+    def advance(self) -> str:
+        self.current += 1
+        return self.source[self.current-1]
+
+
+    def add_token(self, token_type: TokenType, literal=None):
+        text = self.source[self.start:self.current]
+        self.tokens.append(Token(token_type, text, literal, self.line))
+
+
+    def match(self, expected: str) -> bool:
+        if self.is_at_end() or self.source[self.current] != expected:
+            return False
+
+        self.current += 1
+        return True
+
+
+if __name__ == "__main__":
+    Lox.main()
---
+++ token.py
@@ -0,0 +1,14 @@
+from token_type import TokenType
+
+
+class Token:
+
+    def __init__(self, token_type: TokenType, lexeme: str, literal, line: int):
+        self.type = token_type
+        self.lexeme = lexeme
+        self.literal = literal
+        self.line = line
+
+
+    def __str__(self):
+        return "{} {} {}".format(self.type, self.lexeme, self.literal)
