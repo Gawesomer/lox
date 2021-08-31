b587ecf1ef263c8f401f47fcd336cec637446767
Jean-Baptiste Jouband <jean-baptiste@jouband.com>
2021-08-30 20:07:55 -0600

    Add binary arithmetic operator op codes

--- clox/main.c
+++ clox/main.c
@@ -12,6 +12,14 @@ int main(int argc, const char *argv[])
 	init_chunk(&chunk);
 
 	write_constant(&chunk, 1.2, 123);
+
+	write_constant(&chunk, 3.4, 123);
+
+	write_chunk(&chunk, OP_ADD, 123);
+
+	write_constant(&chunk, 5.6, 123);
+
+	write_chunk(&chunk, OP_DIVIDE, 123);
 	write_chunk(&chunk, OP_NEGATE, 123);
 
 	write_chunk(&chunk, OP_RETURN, 123);
--- clox/vm.c
+++ clox/vm.c
@@ -49,6 +49,12 @@ static enum InterpretResult run(void)
 {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+	do { \
+		double b = pop(); \
+		double a = pop(); \
+		push(a op b); \
+	} while (false)
 
 	for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
@@ -73,6 +79,18 @@ static enum InterpretResult run(void)
 			constant = READ_CONSTANT_LONG();
 			push(constant);
 			break;
+		case OP_ADD:
+			BINARY_OP(+);
+			break;
+		case OP_SUBTRACT:
+			BINARY_OP(-);
+			break;
+		case OP_MULTIPLY:
+			BINARY_OP(*);
+			break;
+		case OP_DIVIDE:
+			BINARY_OP(/);
+			break;
 		case OP_NEGATE:
 			push(-pop());
 			break;
@@ -85,6 +103,7 @@ static enum InterpretResult run(void)
 
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 }
 
 enum InterpretResult interpret(struct Chunk *chunk)
