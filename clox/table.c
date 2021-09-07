822ecd52474a4419056717482fe0fc4b640926a7
Jean-Baptiste Jouband <jean-baptiste@jouband.com>
2021-09-07 08:33:20 -0600

    Store Values as keys instead of pointers
    Use NIL_VAL to indicate an unused bucket.
    So tables are not compatible with NIL keys.
    Could create an EMPTY_KEY ValueType in the future to resolve this.

--- clox/table.c
+++ clox/table.c
@@ -31,11 +31,11 @@ uint32_t hash_bytes(const uint8_t *bytes, size_t size)
 	return hash;
 }
 
-static struct Entry *find_entry(struct Entry *entries, int capacity, void *key, uint32_t *hash, size_t size)
+static struct Entry *find_entry(struct Entry *entries, int capacity, Value key, uint32_t *hash, size_t size)
 {
 	uint32_t index;
 
-	index = (hash == NULL) ? hash_bytes(key, size) : *hash;
+	index = (hash == NULL) ? hash_bytes((const uint8_t *)&key, size) : *hash;
 	index %= capacity;
 
 	struct Entry *tombstone = NULL;
@@ -43,7 +43,7 @@ static struct Entry *find_entry(struct Entry *entries, int capacity, void *key, 
 	for (;;) {
 		struct Entry *entry = &entries[index];
 
-		if (entry->key == NULL) {
+		if (IS_NIL(entry->key)) {
 			if (IS_NIL(entry->value)) {
 				// Empty entry.
 				return tombstone != NULL ? tombstone : entry;
@@ -52,7 +52,7 @@ static struct Entry *find_entry(struct Entry *entries, int capacity, void *key, 
 				if (tombstone == NULL)
 					tombstone = entry;
 			}
-		} else if (entry->key == key) {
+		} else if (values_equal(entry->key, key)) {
 			// We found the key.
 			return entry;
 		}
@@ -61,14 +61,14 @@ static struct Entry *find_entry(struct Entry *entries, int capacity, void *key, 
 	}
 }
 
-bool table_get(struct Table *table, void *key, uint32_t *hash, size_t size, Value *value)
+bool table_get(struct Table *table, Value key, uint32_t *hash, size_t size, Value *value)
 {
 	if (table->count == 0)
 		return false;
 
 	struct Entry *entry = find_entry(table->entries, table->capacity, key, hash, size);
 
-	if (entry->key == NULL)
+	if (IS_NIL(entry->key))
 		return false;
 
 	*value = entry->value;
@@ -80,7 +80,7 @@ static void adjust_capacity(struct Table *table, int capacity)
 	struct Entry *entries = ALLOCATE(struct Entry, capacity);
 
 	for (int i = 0; i < capacity; i++) {
-		entries[i].key = NULL;
+		entries[i].key = NIL_VAL;
 		entries[i].value = NIL_VAL;
 	}
 
@@ -88,7 +88,7 @@ static void adjust_capacity(struct Table *table, int capacity)
 	for (int i = 0; i < table->capacity; i++) {
 		struct Entry *entry = &table->entries[i];
 
-		if (entry->key == NULL)
+		if (IS_NIL(entry->key))
 			continue;
 
 		struct Entry *dest = find_entry(entries, capacity, entry->key, &entry->hash, 0);
@@ -104,7 +104,7 @@ static void adjust_capacity(struct Table *table, int capacity)
 	table->capacity = capacity;
 }
 
-bool table_set(struct Table *table, void *key, uint32_t *hash, size_t size, Value value)
+bool table_set(struct Table *table, Value key, uint32_t *hash, size_t size, Value value)
 {
 	if (table->count + 1 > table->capacity * TABLE_MAX_LOAD) {
 		int capacity = GROW_CAPACITY(table->capacity);
@@ -112,9 +112,9 @@ bool table_set(struct Table *table, void *key, uint32_t *hash, size_t size, Valu
 		adjust_capacity(table, capacity);
 	}
 
-	uint32_t computed_hash = (hash == NULL) ? hash_bytes(key, size) : *hash;
+	uint32_t computed_hash = (hash == NULL) ? hash_bytes((const uint8_t *)&key, size) : *hash;
 	struct Entry *entry = find_entry(table->entries, table->capacity, key, &computed_hash, 0);
-	bool is_new_key = entry->key == NULL;
+	bool is_new_key = IS_NIL(entry->key);
 
 	if (is_new_key && IS_NIL(entry->value))
 		table->count++;
@@ -125,7 +125,7 @@ bool table_set(struct Table *table, void *key, uint32_t *hash, size_t size, Valu
 	return is_new_key;
 }
 
-bool table_delete(struct Table *table, void *key, uint32_t *hash, size_t size)
+bool table_delete(struct Table *table, Value key, uint32_t *hash, size_t size)
 {
 	if (table->count == 0)
 		return false;
@@ -133,11 +133,11 @@ bool table_delete(struct Table *table, void *key, uint32_t *hash, size_t size)
 	// Find the entry.
 	struct Entry *entry = find_entry(table->entries, table->capacity, key, hash, size);
 
-	if (entry->key == NULL)
+	if (IS_NIL(entry->key))
 		return false;
 
 	// Place a tombstone in the entry.
-	entry->key = NULL;
+	entry->key = NIL_VAL;
 	entry->value = BOOL_VAL(true);
 	return true;
 }
@@ -147,7 +147,7 @@ void table_add_all(struct Table *from, struct Table *to)
 	for (int i = 0; i < from->capacity; i++) {
 		struct Entry *entry = &from->entries[i];
 
-		if (entry->key != NULL)
+		if (!IS_NIL(entry->key))
 			table_set(to, entry->key, &entry->hash, 0, entry->value);
 	}
 }
@@ -161,17 +161,20 @@ struct ObjString *table_find_string(struct Table *table, const char *chars, int 
 
 	for (;;) {
 		struct Entry *entry = &table->entries[index];
-		struct ObjString *key_str = entry->key;
 
-		if (entry->key == NULL) {
+		if (IS_NIL(entry->key)) {
 			// Stop if we find an empty non-tombstone entry.
 			if (IS_NIL(entry->value))
 				return NULL;
-		} else if (key_str->length == length && entry->hash == hash) {
-			const char *key_chars = (key_str->ptr == NULL) ? key_str->chars : key_str->ptr;
+		} else {
+			struct ObjString *key_str = AS_STRING(entry->key);
 
-			if (memcmp(key_chars, chars, length) == 0)
-				return key_str;  // We found it.
+			if (key_str->length == length && entry->hash == hash) {
+				const char *key_chars = (key_str->ptr == NULL) ? key_str->chars : key_str->ptr;
+
+				if (memcmp(key_chars, chars, length) == 0)
+					return key_str;  // We found it.
+			}
 		}
 
 		index = (index + 1) % table->capacity;
--- clox/table.h
+++ clox/table.h
@@ -5,7 +5,7 @@
 #include "value.h"
 
 struct Entry {
-	void *key;
+	Value key;
 	uint32_t hash;
 	Value value;
 };
@@ -19,9 +19,9 @@ struct Table {
 void init_table(struct Table *table);
 void free_table(struct Table *table);
 uint32_t hash_bytes(const uint8_t *bytes, size_t size);
-bool table_get(struct Table *table, void *key, uint32_t *hash, size_t size, Value *value);
-bool table_set(struct Table *table, void *key, uint32_t *hash, size_t size, Value value);
-bool table_delete(struct Table *table, void *key, uint32_t *hash, size_t size);
+bool table_get(struct Table *table, Value key, uint32_t *hash, size_t size, Value *value);
+bool table_set(struct Table *table, Value key, uint32_t *hash, size_t size, Value value);
+bool table_delete(struct Table *table, Value key, uint32_t *hash, size_t size);
 void table_add_all(struct Table *from, struct Table *to);
 struct ObjString *table_find_string(struct Table *table, const char *chars, int length, uint32_t hash);
 
