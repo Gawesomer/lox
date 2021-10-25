813e81fa46039fa231e8fb134e3ac786b8353010
Jean-Baptiste Jouband <jean-baptiste@jouband.com>
2021-10-25 08:28:51 -0600

    Trace object references to mark all reachable objects

--- clox/memory.c
+++ clox/memory.c
@@ -33,6 +33,8 @@ void mark_object(struct Obj *object)
 {
 	if (object == NULL)
 		return;
+	if (object->is_marked)
+		return;
 #ifdef DEBUG_LOG_GC
 	printf("%p mark ", (void*)object);
 	print_value(OBJ_VAL(object));
@@ -55,6 +57,43 @@ void mark_value(Value value)
 {
 	if (IS_OBJ(value))
 		mark_object(AS_OBJ(value));
+}
+
+static void mark_array(struct ValueArray *array)
+{
+	for (int i = 0; i < array->count; i++)
+		mark_value(array->values[i]);
+}
+
+static void blacken_object(struct Obj *object)
+{
+#ifdef DEBUG_LOG_GC
+	printf("%p blacken ", (void*)object);
+	print_value(OBJ_VAL(object));
+	printf("/n");
+#endif
+
+	switch (object->type) {
+	case OBJ_CLOSURE: {
+		struct ObjClosure *closure = (struct ObjClosure*)object;
+		mark_object((struct Obj*)closure->function);
+		for (int i = 0; i < closure->upvalue_count; i++)
+			mark_object((struct Obj*)closure->upvalues[i]);
+		break;
+	}
+	case OBJ_FUNCTION: {
+		struct ObjFunction *function = (struct ObjFunction*)object;
+		mark_object((struct Obj*)function->name);
+		mark_array(&function->chunk.constants);
+		break;
+	}
+	case OBJ_UPVALUE:
+		mark_value(((struct ObjUpvalue*)object)->closed);
+		break;
+	case OBJ_NATIVE:
+	case OBJ_STRING:
+		break;
+	}
 }
 
 void free_object(struct Obj *object)
@@ -119,6 +158,14 @@ static void mark_roots(void)
 	mark_compiler_roots();
 }
 
+static void trace_references(void)
+{
+	while (vm.gray_count > 0) {
+		struct Obj *object = vm.gray_stack[--vm.gray_count];
+		blacken_object(object);
+	}
+}
+
 void collect_garbage(void)
 {
 #ifdef DEBUG_LOG_GC
@@ -126,6 +173,7 @@ void collect_garbage(void)
 #endif
 
 	mark_roots();
+	trace_references();
 
 #ifdef DEBUG_LOG_GC
 	printf("-- gc end\n");
